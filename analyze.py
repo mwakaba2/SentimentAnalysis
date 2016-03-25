98abb9f15238db271f891ffed8f063ed391eb358
Mariko Wakabayashi <marimarimekko@gmail.com>
2016-03-24 20:22:24 -0500

    file for visualizing sentiment

--- analyze.py
+++ analyze.py
@@ -3,8 +3,7 @@ import time
 
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
-import plotly.plotly as py
-import plotly.graph_objs as go
+
 
 month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 monthly_pos = [0]*12
@@ -22,8 +21,6 @@ with open('fb_comments_data.json') as data_file:
 		neg_result = sentiment_results.p_neg
 		month = time.localtime(comment_time).tm_mon - 1
 
-		print(monthly_pos)
-		print(month)
 		monthly_pos[month]+=pos_result
 		count[month]+=1
 		monthly_neg[month]+=neg_result
@@ -36,31 +33,3 @@ with open('fb_comments_data.json') as data_file:
 
 # yearly analysis
 
-# trace0 = go.Bar(
-#     x=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
-#        'Nov', 'Dec'],
-#     y=[20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17],
-#     name='Primary Product',
-#     marker=dict(
-#         color='rgb(49,130,189)'
-#     )
-# )
-# trace1 = go.Bar(
-#     x=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
-#        'Nov', 'Dec'],
-#     y=[19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16],
-#     name='Secondary Product',
-#     marker=dict(
-#         color='rgb(204,204,204)',
-#     )
-# )
-# data = [trace0, trace1]
-# layout = go.Layout(
-#     xaxis=dict(
-#         # set x-axis' labels direction at 45 degree angle
-#         tickangle=-45,
-#     ),
-#     barmode='group',
-# )
-# fig = go.Figure(data=data, layout=layout)
-# plot_url = py.plot(fig, filename='angled-text-bar')
